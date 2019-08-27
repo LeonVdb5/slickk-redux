998e6c6832d205e11447c0f06e20881b14c38a68
Leon <leon.vdb5@gmail.com>
2019-08-27 13:35:51 -0400

    navbar changes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,7 @@ import '../containers/VisiblePostList.css'
 
 const ProductList = (props) => (
 	(props.products) ?
-		<div className="scroll-box" style={{overflowY: 'scroll', height: '100%', bottom: '25px'}}>
+		<div className="scroll-box" style={{overflowY: 'scroll', height: '100%'}}>
 			<div className="post-components">
 				{props.products.map((product, i) => (
 					<ProductCard product={product} key={i}/>
