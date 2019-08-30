9f4b1dee2b637bc9e602de7c29ed34894191c075
Leon <leon.vdb5@gmail.com>
2019-08-30 17:19:15 -0400

    register and filter functionality updated

--- src/containers/SearchLink.js
+++ src/containers/SearchLink.js
@@ -1,27 +1,27 @@
-import {connect} from 'react-redux';
-import {  } from '../actions/index'; 
-import Link from '../components/Link';
+// import {connect} from 'react-redux';
+// import {  } from '../actions/index'; 
+// import Link from '../components/Link';
 
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-	onClick: () => {
-		switch(ownProps.type){
-			case "hold":
-				dispatch(setHoldFilter(ownProps.filter))
-				break;
-			case "shine":
-				dispatch(setShineFilter(ownProps.filter))
-				break;
-			case "hairlength":
-				dispatch(setHairlengthFilter(ownProps.filter))
-				break;
-			case "hairtype":
-				dispatch(setHairtypeFilter(ownProps.filter))
-				break;
-			default:
-				throw new Error ("no type")
-		}
-	}
-})
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+// 	onClick: () => {
+// 		switch(ownProps.type){
+// 			case "hold":
+// 				dispatch(setHoldFilter(ownProps.filter))
+// 				break;
+// 			case "shine":
+// 				dispatch(setShineFilter(ownProps.filter))
+// 				break;
+// 			case "hairlength":
+// 				dispatch(setHairlengthFilter(ownProps.filter))
+// 				break;
+// 			case "hairtype":
+// 				dispatch(setHairtypeFilter(ownProps.filter))
+// 				break;
+// 			default:
+// 				throw new Error ("no type")
+// 		}
+// 	}
+// })
 
-export default connect(null, mapDispatchToProps)(Link)
+// export default connect(null, mapDispatchToProps)(Link)
