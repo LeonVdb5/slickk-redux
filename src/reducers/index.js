007024250e3573e71ae854325a5217d2e8637c38
Philip Kutlesa <philip.kutlesa@gmail.com>
2019-08-31 15:38:33 -0400

    updated login, actions, and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { requestPosts } from './fetching.js'
+import { requestPosts, auth } from './fetching.js'
 import { ProductHoldFilter, ProductShineFilter, HairLengthFilter, HairTypeFilter } from './filter.js'
 import { combineReducers } from 'redux';
 
@@ -6,6 +6,7 @@ import { combineReducers } from 'redux';
 
 export const rootReducers = combineReducers({
   requestPosts, 
+  auth,
   ProductHoldFilter, 
   ProductShineFilter, 
   HairLengthFilter, 
